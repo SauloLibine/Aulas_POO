d7f44811e1e875782f32072e3f22b672ad2f0318
SauloLibine <sauloolibne@gmail.com>
2025-11-10 10:22:04 -0300

    Adicionado atividades do pc institucional

---
+++ sistema_radar/Radar.java
@@ -0,0 +1,43 @@
+package sistema_radar;
+
+public class Radar {
+    
+    private String localizacao;
+    private Integer limiteVelocidade;
+
+    public Radar(String localizacao, Integer limiteVelocidade){
+        this.localizacao = localizacao;
+        this.limiteVelocidade = limiteVelocidade;
+    }
+
+    public String getLocalizacao(){
+        return this.localizacao;
+    }
+
+    public void setLocalizacao(String localizacao){
+        this.localizacao = localizacao;
+    }
+
+    public Integer getLimiteVelocidade(){
+        return this.limiteVelocidade;
+    }
+
+    public void setLimiteVelocidade(Integer limiteVelocidade){
+        this.limiteVelocidade = limiteVelocidade;
+    }
+
+    public void emitirNotificacao(String placa, Integer velocidadeObservada){
+        System.out.println("Placa: "+placa);
+        System.out.println("Velocidade observada: "+velocidadeObservada);
+        System.out.println("Localização: "+ this.localizacao);
+        System.out.println("Velocidade da via: "+ this.limiteVelocidade);
+        
+    }
+
+    public void avaliarVelocidade(Carro carro){
+        if(carro.getVelocidade() > this.limiteVelocidade){
+            emitirNotificacao(carro.getPlaca(), carro.getVelocidade());
+        }
+    }
+
+}
---
+++ sistema_radar/Simulacao.java
@@ -0,0 +1,22 @@
+package sistema_radar;
+
+public class Simulacao {
+    public static void main(String[] args) {
+        Carro opala = new Carro("ACLR300", 0, 76, "Comodoro");
+
+        Radar radar = new Radar("Pistão Sul", 60);
+
+        radar.avaliarVelocidade(opala);
+
+        opala.acelerar(); // 10
+        opala.acelerar();
+        opala.acelerar();
+        opala.acelerar();
+        opala.acelerar();
+        opala.acelerar();
+        opala.acelerar(); // 70
+
+        opala.setVelocidade(-60);
+        radar.avaliarVelocidade(opala);
+    }
+}
